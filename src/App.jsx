7b3d0de9e9d2cb236eb82537f5e956ff00a2e097
Prince Khunt <princekhuntdatastructure@gmail.com>
2023-07-14 15:02:22 +0530

    Start to build

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./App.scss";
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
