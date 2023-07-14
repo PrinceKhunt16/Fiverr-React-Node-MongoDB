e0a5f5c060e770eeb85e33d5a05ca7107a4f14ee
Prince Khunt <princekhuntdatastructure@gmail.com>
2023-07-14 15:59:03 +0530

    Navbar done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import "./Navbar.scss";
 
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
+  const { pathname } = useLocation();
+  const currentUser = {
+    id: 1,
+    username: "John Doe",
+    isSeller: true
+  };
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    }
+  }, []);
+
   return (
-    <div className='navbar'>
+    <div className={active || pathname !== '/' ? 'navbar active' : "navbar"}>
       <div className="container">
         <div className="logo">
-          <span className='text'>fiverr</span>
+          <Link className='link' to={'/'}>
+            <span className='text'>fiverr</span>
+          </Link>
           <span className='dot'>.</span>
         </div>
         <div className="links">
@@ -14,10 +38,85 @@ const Navbar = () => {
           <span>Explore</span>
           <span>English</span>
           <span>Sign in</span>
-          <span>Become a Seller</span>
-          <button>Join in</button>
+          {!currentUser.isSeller &&
+            <span>Become a Seller</span>
+          }
+          {!currentUser &&
+            <button>Join in</button>
+          }
+          {currentUser ? (
+            <div className="user" onClick={() => setOpen(!open)}>
+              <img
+                src="https://learnyzen.com/wp-content/uploads/2017/08/test1-481x385.png"
+                alt=""
+              />
+              <span>{currentUser?.username}</span>
+              {open && <div className="options">
+                {currentUser.isSeller && (
+                  <>
+                    <Link className="link" to="/mygigs">
+                      Gigs
+                    </Link>
+                    <Link className="link" to="/add">
+                      Add New Gig
+                    </Link>
+                  </>
+                )}
+                <Link className="link" to="/orders">
+                  Orders
+                </Link>
+                <Link className="link" to="/messages">
+                  Messages
+                </Link>
+                <Link className="link" to="/">
+                  Logout
+                </Link>
+              </div>}
+            </div>
+          ) : (
+            <>
+              <span>Sign in</span>
+              <Link className="link" to="/register">
+                <button>Join</button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
+      {active || pathname !== '/' && (
+        <>
+          <hr />
+          <div className="menu">
+            <Link className="link menuLink" to="/">
+              Graphics & Design
+            </Link>
+            <Link className="link menuLink" to="/">
+              Video & Animation
+            </Link>
+            <Link className="link menuLink" to="/">
+              Writing & Translation
+            </Link>
+            <Link className="link menuLink" to="/">
+              AI Services
+            </Link>
+            <Link className="link menuLink" to="/">
+              Digital Marketing
+            </Link>
+            <Link className="link menuLink" to="/">
+              Music & Audio
+            </Link>
+            <Link className="link menuLink" to="/">
+              Programming & Tech
+            </Link>
+            <Link className="link menuLink" to="/">
+              Business
+            </Link>
+            <Link className="link menuLink" to="/">
+              Lifestyle
+            </Link>
+          </div>
+        </>
+      )}
     </div>
   )
 }
